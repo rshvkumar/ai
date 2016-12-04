e7c281ec3b69d954d0b47b1342c59515502074d5
Keshav Saharia <keshav@techlabeducation.com>
2016-12-04 14:36:20 -0800

    Update Player.java

--- evolution/Player.java
+++ evolution/Player.java
@@ -7,6 +7,12 @@ public class Player {
 	
 	Genome genome;
 	
+	public Player() {}
+	
+	public Player(Genome genome) {
+		this.genome = genome;
+	}
+	
 	public void move(Game game, int piece) {
 		double[] input = new double[9];
 		
@@ -47,10 +53,61 @@ public class Player {
 }
 
 class HumanPlayer extends Player {
+	static boolean windowInitialized = false;
+	
+	public HumanPlayer() {
+		if (! windowInitialized) {
+			windowInitialized = true;
+			Window.size(600, 600);
+			Window.out.font("Courier", 150);
+			Window.frame();
+		}
+	}
+	
 	public void move(Game game, int piece) {
-		// Draw the board, wait for a click
-		Window.mouse.waitForClick();
-		
-		Window.mouse.waitForRelease();
+		while (true) {
+			Window.out.background("white");
+			
+			Window.out.color("gray");
+			Window.out.rectangle(200, 300, 5, 580);
+			Window.out.rectangle(400, 300, 5, 580);
+			Window.out.rectangle(300, 200, 580, 5);
+			Window.out.rectangle(300, 400, 580, 5);
+			
+			// Draw the board, wait for a click
+			Window.out.color("black");
+			for (int x = 0 ; x < 3 ; x++) {
+				for (int y = 0 ; y < 3 ; y++) {
+					if (game.valueOf(x, y) == 1) {
+						Window.out.print("X", x * 200 + 50, y * 200 + 150);
+					}
+					if (game.valueOf(x, y) == 2) {
+						Window.out.print("O", x * 200 + 50, y * 200 + 150);
+					}
+				}
+			}
+			
+			// Check the mouse position
+			int x = Window.mouse.getX() / 200;
+			int y = Window.mouse.getY() / 200;
+			
+			// Draw a shadow of a piece on the square
+			if (game.valid(x, y)) {
+				Window.out.color("light gray");
+				if (piece == 1) {
+					Window.out.print("X", x * 200 + 50, y * 200 + 150);
+				}
+				if (piece == 2) {
+					Window.out.print("O", x * 200 + 50, y * 200 + 150);
+				}
+			}
+			
+			if (Window.mouse.clicked() && game.valid(x, y)) {
+				 game.place(x, y, piece);
+				 Window.mouse.waitForRelease();
+				 break;
+			}
+			Window.frame();
+		}
 	}
 }
